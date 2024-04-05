995776fa42f2b40b03ea2454fcc49703e4f65c96
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-05 21:38:35 +0100

    Création du contexte de la base de données

---
+++ DAL/LibraryContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using TP06.Models;
+
+namespace TP06.DAL
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext() : base("LibraryContext")
+        { }
+        
+        public DbSet<Categorie> Categories { get; set; }
+        public DbSet<Ouvrage> Ouvrages { get; set;}
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
