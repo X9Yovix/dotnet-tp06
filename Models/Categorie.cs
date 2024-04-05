e16bfaa3db9774504d50666f90883ed302086af2
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-05 21:26:25 +0100

    Création des classes du domaine (Model) & Installation du data entity framework

---
+++ Models/Categorie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP06.Models
+{
+    public class Categorie
+    {
+        public string Id { get; set; }
+        public string Nom { get; set; }
+        public virtual ICollection<Ouvrage> Ouvrages { get; set; }
+    }
+}
---
+++ Models/Ouvrage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP06.Models
+{
+    public class Ouvrage
+    {
+        public string Id { get; set; }
+        public string Titre { get; set;}
+        public string Descriptif { get; set;}
+        public decimal Prix { get; set; }
+        public virtual Categorie Categorie { get; set; }
+    }
+}
